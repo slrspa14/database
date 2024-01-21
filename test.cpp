1b04018a3951f28062096bfcc7b9975366368cd2
slrspa14 <kw32482002@gmail.com>
2024-01-21 21:00:22 +0900

    saramin

--- test.cpp
+++ test.cpp
@@ -7,23 +7,69 @@
 #include <cstdlib>
 #include <ctime>
 
-void start_page();
+void announcement(); //랜덤공고
 void start_image();
-int insert(std::string id, std::string pw, std::string email, std::string pnum);
-int join_page();
+void join_page();
+void search();
 int callback(void *, int, char **, char **);
-int view(void);
+void log_in();
+void menu();
+
+int log_count = 0;
 
 int main()
 {
-    // start_image();
-    // start_page();
-    join_page();
-
+    menu();
     return 0;
 }
 
-int join_page() // 회원가입 페이지
+void menu()
+{
+    announcement();
+    int choice = 0;
+    
+
+    while(choice < 4)
+    {
+        std::cout << "============================" << std::endl;
+        std::cout << "1. 로그인" << std::endl;
+        std::cout << "2. 회원가입" << std::endl;
+        std::cout << "3. 검색" << std::endl;
+        std::cout << "4. 종료" << std::endl;
+        std::cout << "============================" << std::endl;
+        std::cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            if(log_count==1)
+            {
+                std::cout << "로그인 상태입니다." << std::endl;
+                sleep(1);
+                system("clear");
+                break;
+            }
+            log_in();
+            break;
+        case 2:
+            if(log_count == 1)
+            {
+                std::cout << "로그인 상태입니다." << std::endl;
+                sleep(1);
+                system("clear");
+                break;
+            }
+            join_page();
+            break;
+        case 3:
+            search();
+            break;
+        case 4:
+            break;
+        }
+    }
+}
+
+void join_page() // 회원가입 페이지
 {
     system("clear");
     sqlite3 *db; //핸들, 파일디스크립터같은
@@ -44,7 +90,7 @@ int join_page() // 회원가입 페이지
         std::cin >> join_id;
         while(1)
         {
-            join = " SELECT ID FROM 'join' WHERE ID = '" + join_id +"'; "; // 입력한 join_id랑 쿼리문 비교
+            join = " SELECT ID FROM USER_DATA WHERE ID = '" + join_id +"'; "; // 입력한 join_id랑 쿼리문 비교
             rc = sqlite3_prepare_v2(db, join.c_str(), -1, &res, 0); // 준비
             rc = sqlite3_step(res); // 실행하기
             if(rc == SQLITE_ROW) // db랑 사용자 입력이 같다면 중복검사
@@ -78,7 +124,7 @@ int join_page() // 회원가입 페이지
         std::cin >> join_email;
         while(1)
         {
-            join = "SELECT EMAIL FROM 'join' WHERE EMAIL = '"+ join_email + "';";
+            join = "SELECT EMAIL FROM USER_DATA WHERE EMAIL = '"+ join_email + "';";
             rc = sqlite3_prepare_v2(db, join.c_str(), -1, &res, 0);
             rc = sqlite3_step(res);
             if(rc == SQLITE_ROW) // db랑 사용자 입력이 같다면 중복검사
@@ -108,7 +154,7 @@ int join_page() // 회원가입 페이지
         }
         while(1)
         {
-            join = "SELECT PNUM FROM 'join' WHERE PNUM = '"+ join_pnum + "';";
+            join = "SELECT PNUM FROM USER_DATA WHERE PNUM = '"+ join_pnum + "';";
 
             rc = sqlite3_prepare_v2(db, join.c_str(), -1, &res, 0);
             rc = sqlite3_step(res);
@@ -125,20 +171,21 @@ int join_page() // 회원가입 페이지
         }
         break;
     }
-    
-    join = "INSERT INTO 'join' VALUES('"+join_id+"','"+join_pw+"','"+join_email+"','"+join_pnum+"');"; //쿼리문 보내고
+    join = "INSERT INTO USER_DATA(USER_ID, PW, EMAIL, PNUM) VALUES('"+ join_id +"','"+ join_pw +"','"+ join_email +"','"+ join_pnum +"');"; //쿼리문 보내고
     rc = sqlite3_prepare_v2(db, join.c_str(), -1, &res, 0);// 준비
     rc = sqlite3_step(res); // 실행
 
     sqlite3_finalize(res); //SQL 쿼리 핸들 정리
+    
     sqlite3_close(db); //DB 닫기
+
+    system("clear");
     std::cout << "회원가입이 완료되었습니다." << std::endl;
     std::cout << "회원가입내역" << std::endl;
-    std::cout << join_id << std::endl;
-    std::cout << join_pw << std::endl;
-    std::cout << join_email << std::endl;
-    std::cout << join_pnum << std::endl;
-    return 0;
+    std::cout << "ID:" << join_id << std::endl;
+    std::cout << "PW:" << join_pw << std::endl;
+    std::cout << "e-mail:" <<join_email << std::endl;
+    std::cout << "PNUM:" << join_pnum << std::endl;
 }
 
 void log_in()
@@ -150,109 +197,44 @@ void log_in()
 
     std::cout << "로그인페이지" << std::endl;
     std::string log_id, log_pw, join;
+    int pw;
     int rc = sqlite3_open("/home/aiot11/Downloads/worknet", &db); //열고
 
     std::cout << "ID:";
     std::cin >> log_id;
     std::cout << "PW:";
-    std::cin >> log_pw;
+    std::cin >> pw;
+    log_pw = std::to_string(pw);
+
     while(1)
     {
-        join = " SELECT ID FROM 'join' WHERE ID = '" + log_id +"' AND '"+ log_pw +"';"; // 입력한 join_id랑 쿼리문 비교
+        join = "SELECT USER_ID, PW FROM USER_DATA WHERE USER_ID = '"+ log_id +"' AND PW = '"+ log_pw + "';";
+        sqlite3_stmt* res;
         rc = sqlite3_prepare_v2(db, join.c_str(), -1, &res, 0); // 준비
-        rc = sqlite3_step(res); // 실행하기
-        if(rc == SQLITE_ROW) // db랑 사용자 입력이 같다면 중복검사
+         // 실행하기
+
+        if(sqlite3_step(res) == SQLITE_ROW) // db랑 사용자 입력이 같다면
         {
-            std::cout << "로그인 성공" << std::endl;
-            std::cout << "공고페이지로 이동하기" << std::endl;
+            system("clear");
+            std::cout << "로그인 되었습니다." << std::endl;
+            sleep(2);
+            log_count++;
+            break;
         }
         else
         {
+            system("clear");
             std::cout << "일치하는 ID 및 PW가 없습니다." << std::endl;
             std::cout << "다시 입력해주세요." << std::endl;
             std::cout << "ID:";
             std::cin >> log_id;
-            std::cout << "PW";
+            std::cout << "PW:";
             std::cin >> log_pw;
         }
     }
-
-}
-int insert(std::string id, std::string pw, std::string email, std::string pnum)
-{
-    sqlite3 *db;
-    char *err_msg = 0;
-    
-    int rc = sqlite3_open("/home/aiot11/Downloads/worknet", &db);
-    
-    if (rc != SQLITE_OK)
-    {
-        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);
-        
-        return 1;
-    }
-
-    std::string sql = "INSERT INTO 'join' VALUES('"+id+"', '"+pw+"', '"+email+"', '"+pnum+"');";
-
-    rc = sqlite3_exec(db, sql.c_str(), 0, 0, &err_msg);
-    
-    if (rc != SQLITE_OK )
-    {
-        fprintf(stderr, "SQL error: %s\n", err_msg);
-        
-        sqlite3_free(err_msg);        
-        sqlite3_close(db);
-        
-        return 1;
-    }
-    sqlite3_close(db);
-    
-    return 0;
 }
 
-
-int view(void)
-{
-    sqlite3 *db;
-    char *err_msg = 0;
-    
-    int rc = sqlite3_open("/home/aiot11/Downloads/worknet", &db);
-    
-    if (rc != SQLITE_OK)
-    {
-        fprintf(stderr, "Cannot open database: %s\n", 
-                sqlite3_errmsg(db));
-        sqlite3_close(db);
-        
-        return 1;
-    }
-    
-    const char *sql = "select * from resume";
-        
-    rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
-    
-    if (rc != SQLITE_OK )
-    {
-        fprintf(stderr, "Failed to select data\n");
-        fprintf(stderr, "SQL error: %s\n", err_msg);
-
-        sqlite3_free(err_msg);
-        sqlite3_close(db);
-        
-        return 1;
-    } 
-    
-    sqlite3_close(db);
-    
-    return 0;
-}
-
-int callback(
-    void *NotUsed,
-    int argc,
-    char **argv, 
-    char **azColName)
+int callback(void *NotUsed,int argc,char **argv, char **azColName)
 {    
     NotUsed = 0;
     
@@ -278,26 +260,82 @@ void start_image()
     system("clear");
 }
 
-void start_page()
+void search()
 {
-    std::cout << "번호를 선택해주세요." << std::endl;
-    std::cout << "1. 로그인" << std::endl;
-    std::cout << "2. 회원가입" << std::endl;
-    int choice = 0;
-    std::cin >> choice;
-    if(choice == 1)
+    system("clear");
+    announcement();
+    sqlite3 *db; //핸들, 파일디스크립터같은
+    char *err_msg = 0; // 오류메시지
+    sqlite3_stmt* res;
+
+    //db 열고 입력값이랑 db저장된 갑이랑 비교하고 공고 출력해주기
+    std::cout << "\t검색창" << std::endl;
+    std::cout << "키워드를 입력해주세요." << std::endl;
+    std::cout << "ex) C++, C, C#" << std::endl;
+    std::string user_search;
+    std::cin >> user_search;
+
+
+    int rc = sqlite3_open("/home/aiot11/Downloads/worknet", &db); //db 열기
+
+    if (rc != SQLITE_OK) // 오픈에러
     {
-        std::cout << "로그인 페이지로 이동" << std::endl;
-        // log_in();
+        fprintf(stderr, "Cannot open database: %s\n", 
+                sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(1);
     }
-    else if(choice == 2)
+
+    std::string search = "SELECT 회사명, 경력, 학력, 필요스킬 FROM resume WHERE 필요스킬 = '"+ user_search + "';";
+    
+    rc = sqlite3_exec(db, search.c_str(), callback, 0, &err_msg); //쿼리문 전달 및 실행
+    if(rc != SQLITE_OK)
     {
-        std::cout << "회원가입 페이지로 이동" << std::endl;
-        join_page();
+        std::cout << "error" << std::endl;
+        exit(1);
     }
-    else
+
+    std::cout << "공고상세정보 보기를 원하시면 회사명을 입력해주세요" << std::endl;
+    std::cout << "검색:";
+    std::string ser1;
+    std::cin >> ser1;
+    std::string detail_search = "SELECT * FROM details WHERE 회사명 = '"+ ser1 +"';";
+    sleep(1);
+
+    rc = sqlite3_exec(db, ser1.c_str(), callback, 0, &err_msg); //쿼리문 전달 및 실행
+
+    if(rc != SQLITE_OK)
     {
-        std::cout << "잘못된 선택입니다. 다시 선택해주세요" << std::endl;   
+        std::cout << "error" << std::endl;
+        exit(1);
     }
+
+    sqlite3_close(db); //DB 닫기
+
 }
 
+void announcement()
+{
+    sqlite3 *db; //핸들, 파일디스크립터같은
+    char *err_msg = 0; // 오류메시지
+
+    int rc = sqlite3_open("/home/aiot11/Downloads/worknet", &db); //db 열기
+
+    if (rc != SQLITE_OK) // 오픈에러
+    {
+        fprintf(stderr, "Cannot open database: %s\n", 
+                sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(1);
+    }
+
+    std::string random_announcement = "SELECT * FROM resume ORDER BY RANDOM() LIMIT 2;";
+
+    rc = sqlite3_exec(db, random_announcement.c_str(), callback, 0, &err_msg); //쿼리문 전달 및 실행
+    if(rc != SQLITE_OK)
+    {
+        std::cout << "error" << std::endl;
+        exit(1);
+    }
+    sqlite3_close(db); //DB 닫기
+}
